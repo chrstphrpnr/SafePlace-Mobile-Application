270d54890be6e0b00ea8c6e20debac5ea4e9baf6
Christopher Panerio <christopherpaneriotup@gmail.com>
2022-11-14 12:50:52 +0800

    DatePicker

--- app/src/main/java/org/tup/safeplace/Constants/API.java
+++ app/src/main/java/org/tup/safeplace/Constants/API.java
@@ -1,9 +1,8 @@
 package org.tup.safeplace.Constants;
 
 public class API {
-
     //API URL
-    public static String URL = "http://192.168.30.27:8080/";
+    public static String URL = "http://192.168.29.27:8080/";
     public static String API = URL + "api/";
 
     //Hospital List
@@ -26,7 +25,5 @@ public class API {
 
     //User Authentication Register User Info
     public static String save_user_info = API + "save_user_info/";
-
-
 
 }
---
+++ app/src/main/java/org/tup/safeplace/TODO.java
@@ -0,0 +1,9 @@
+package org.tup.safeplace;
+
+public class TODO {
+    //TODO: Swipe Down to Refresh in all Barangay, Hospital, and Police Station ListView
+    //TODO: Update User Personal Information
+    //TODO: Show User Name, Email, and Verification Status in the Drawable Drawer
+}
+
+
